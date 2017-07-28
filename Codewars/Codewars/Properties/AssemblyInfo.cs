587e7195f3e950b4d75c7f6288efde179fe0ccfd
TitanDarren <sqz777isme@gmail.com>
2017-07-28 16:14:36 +0800

    initial

---
+++ Codewars/Codewars/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Codewars")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Titansoft")]
+[assembly: AssemblyProduct("Codewars")]
+[assembly: AssemblyCopyright("Copyright © Titansoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e303a29-9e1b-4029-bd30-cd6ac23110bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
