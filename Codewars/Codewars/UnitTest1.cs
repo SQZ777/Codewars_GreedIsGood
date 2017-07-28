146815b14fa7f09b837f21fc6ae37c717ab1fbdc
TitanDarren <sqz777isme@gmail.com>
2017-07-28 16:17:58 +0800

    fail the first test case

--- Codewars/Codewars/UnitTest1.cs
+++ Codewars/Codewars/UnitTest1.cs
@@ -7,8 +7,21 @@ namespace Codewars
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Input_23462_Should_Be_0()
         {
+            var kata = new Kata();
+            var input = new []{ 2,3,4,5,6 };
+            var actual = kata.Score(input);
+            var expected = 0;
+            Assert.AreEqual(expected,actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int Score(int[] dice)
+        {
+            throw new NotImplementedException();
         }
     }
 }
